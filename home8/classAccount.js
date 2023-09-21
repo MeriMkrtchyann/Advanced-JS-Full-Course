5b1b199c53df3e17bab3d9eb8b87b06c139a3f95
Meri Mkrtchyan <meri.mkrtchyan1999@gmail.com>
2023-09-21 21:04:22 +0400

    Add task classAccount i made changes in the methods and add new metod submitBalance

--- home8/classAccount.js
+++ home8/classAccount.js
@@ -1,50 +1,78 @@
 class Account {
 
     #balance = 0
+    #id
     
     constructor ( name , balance){
         this.name = name
         this.balance = balance
-        this.id = Account.generateUniqueId()
+        this.#id = Account.generateUniqueId()
     }
 
     get balance(){
         return this.#balance
     }
 
-    set balance(balance){
-        this.#balance = balance
+    set balance(nowbalance){
+        if (typeof nowbalance === 'number' && nowbalance > 0){
+            this.#balance = nowbalance
+        }else{
+            console.log("Please enter positive number only")
+        }
     }
 
     credit (value){
-        return this.balance += value
+        if (typeof nowbalance === 'number' && nowbalance > 0){
+            this.balance += value
+        } else {
+            console.log("Please enter positive number only")
+        }
+        
     }
 
     debit (value){
-        if (this.balance < value){
-            return "insufficient funds"
+        if (typeof nowbalance === 'number' &&  value > 0){
+           if (this.balance >= value){
+                this.balance -= value
+           }else {
+                console.log("Insufficient funds.")
+           }
+        }else {
+            console.log("Please enter positive number only")
         }
-        return this.balance -= value
+        
     }
 
     transferTo (acaunt , amount){
-        if (this.balance < amount){
-            return "insufficient funds"
+        if (typeof amount === "number" && amount > 0){
+            if (this.balance >= amount){
+                this.balance -= amount
+                acaunt.credit(amount)
+            } else {
+                conseol.log ("insufficient funds")
+            }
+        }else {
+            console.log("Please enter positive number only")
         }
-        this.balance -= amount
-        acaunt.credit(amount)
+        
+        
     }
 
     static generateUniqueId (){
        return Date.now().toString(36) + Math.random().toString(36).substring(2,5)
     }
-    
+
     static identifyAccounts (account1 ,account2 ){
-        console.log(account1.id)
-        console.log(account2.id)
-        return account1.id === account2.id
+        return account1.#id === account2.#id
     }
-    
+
+    submitBalance(password){
+        if (password === "hello"){
+            console.log(this.balance)
+        } else {
+            console.log("enter the correct password")
+        }
+    }
 }
 
 const saving = new Account("saving", 1000);
@@ -55,9 +83,12 @@ console.log(saving.debit(2000));
 console.log(saving.transferTo(current, 1000));
 console.log(saving.balance);
 console.log(current.balance);
-const res = Account.identifyAccounts(current, saving);
+const res = Account.identifyAccounts(current ,saving);
 console.log(res)
 
+saving.balance = "hello";
+saving.submitBalance("hello");
+console.log(saving);
 
 
 
