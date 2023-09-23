6fd82644bdae69ff2231d60fed41ad98991bcb08
Meri Mkrtchyan <meri.mkrtchyan1999@gmail.com>
2023-09-23 13:39:53 +0400

    Add chanages in gameShiratori.js file

--- home8/gameShiratori.js
+++ home8/gameShiratori.js
@@ -4,41 +4,35 @@ class Shiritory {
     game_over = false
 
     play(word) {
-
-        if (!(this.words.length)){
-            this.words.push(word)
-            return this.words
+        if (word.length > 0){
+            if (this.words.length === 0 || this.words[this.words.length - 1].slice(-1) === word[0] && !(this.words.includes(word))){
+                this.words.push(word)
+                return this.words
+            }
+            
+            this.game_over = true
+            return "game over"
         }
-
-        if ( this.words[this.words.length - 1].slice(-1) === word[0] && !(this.words.includes(word))){
-            this.words.push(word)
-            return this.words
-        }
-
-        this.game_over = true
         return "game over"
     }
 
     restart() {
         this.words = []
         this.game_over = false
-        return "game restarted"
-           
+        return "game restarted"      
     }
 }
 
 let myShiritori = new Shiritory();
 
-myShiritori.play("apple"); // ["apple"]
-myShiritori.play("ear"); // ["apple", "ear"]
-myShiritori.play("rhino"); // ["apple", "ear", "rhino"]
-myShiritori.play("corn"); // "game over"
+console.log(myShiritori.play("apple")); // ["apple"]
+console.log(myShiritori.play("ear")); // ["apple", "ear"]
+console.log(myShiritori.play("rhino")); // ["apple", "ear", "rhino"]
+console.log(myShiritori.play("corn")); // "game over"
 
-myShiritori.words; // ["apple", "ear", "rhino"]
-// Words should be accessible.
+console.log(myShiritori.words); // ["apple", "ear", "rhino"]
 myShiritori.restart(); // "game restarted"
-myShiritori.words; // []
-// Words array should be set back to empty.
-myShiritori.play("hostess"); // ["hostess"]
-myShiritori.play("stash"); // ["hostess", "stash"]
-myShiritori.play("hostess"); // "game over"
+console.log(myShiritori.words); // []
+console.log(myShiritori.play("hostess")); // ["hostess"]
+console.log(myShiritori.play("stash")); // ["hostess", "stash"]
+console.log(myShiritori.play("hostess")); // "game over"
