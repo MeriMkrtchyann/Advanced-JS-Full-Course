7c32535a002eb442d3413d84bab839c34c017c09
Meri Mkrtchyan <meri.mkrtchyan1999@gmail.com>
2023-09-26 16:44:06 +0400

    add task formatDate in the home9

--- home9/formatDate.js
+++ home9/formatDate.js
@@ -1,23 +1,9 @@
 const formatDate = (date) => {
 
-    const months = [
-        "January",
-        "February",
-        "March",
-        "April",
-        "May",
-        "June",
-        "July",
-        "August",
-        "September",
-        "October",
-        "November",
-        
-        "December",
-      ];
+    const months = ["January", "February", "March", "April", "May", "June", "July", 
+                    "August", "September", "October", "November", "December", ];
 
-      return date.getDate() + " " + months[date.getMonth()] + " " + date.getFullYear()
-    
+      return date.getDate() + " " + months[date.getMonth()] + " " + date.getFullYear()   
 }
     
 console.log("Actual output: ", formatDate(new Date("2020-05-14")));
