ef980319096b81a794a21874346698c683c6815b
Meri Mkrtchyan <meri.mkrtchyan1999@gmail.com>
2023-11-02 17:05:45 +0400

    change task BabyConstructor.js

--- home6/BabyConstructor.js
+++ home6/BabyConstructor.js
@@ -1,16 +1,7 @@
-/*
-Write a Baby constructor subclassing Person.
-Besides name and age, Baby takes a third argument to initialize favoriteToy.
-Besides the methods on Person.prototype, babies have the ability to .play():
-Should return a string "Playing with x", x being the favorite toy.
-*/
-
 function Baby (name , age , favoriteToy){
-
     this.name = name;
     this.age = age;
     this.favoriteToy = favoriteToy;
-
 }
 
 Baby.prototype.play = function (){
