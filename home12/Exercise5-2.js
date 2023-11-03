c6b637bb504985caa71f66360fcbe4aac86236a4
Meri Mkrtchyan <meri.mkrtchyan1999@gmail.com>
2023-11-03 18:06:38 +0400

    add task Exercise5-2.js in the home12

---
+++ home12/Exercise5-2.js
@@ -0,0 +1,23 @@
+Promise.resolve()
+    .then(data => {
+        throw new Error('Api Error');
+        return 1;
+    })
+    .then(data => console.log('ok'))
+    .catch(error => {
+        console.log(error.message);
+        return "2";
+    })
+    .then(data => {
+        console.log(data);
+    })
+
+/*
+    Կաշխատի Promise.resolve() կտեսնի .then() և .then()-ի callBack-y Event Loop -> Microtask Queue [anonymous]
+    Event Loop-ը Microtask Queue [anonymous] կբերի anonymous կվերադարձնի new Error
+    Քանի որ Error է մտնի .catch() կտպի new Error-ի message Api Error ու կվերադարձնի "2"
+    Կմտնի .then() կտպի 2 և կոդի աշխատանքը կավարտվի
+
+    Կտպի ։ Api Error  2
+
+*/
